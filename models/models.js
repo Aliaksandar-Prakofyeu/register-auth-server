1b0a45ef77df63872d350a118376d7b78d5b76da
Brusnicka <yerbamaniac@gmail.com>
2023-03-25 00:13:33 +0400

    Fixed model

--- models/models.js
+++ models/models.js
@@ -6,9 +6,12 @@ const User = sequelize.define('user', {
     name: {type: DataTypes.STRING},
     email: {type: DataTypes.STRING, unique: true},
     password: {type: DataTypes.STRING},
-    status: {type: DataTypes.STRING, defaultValue: 'active'},
-    registration_time:{type: DataTypes.DATE, allowNull: false},
-    last_login_time:{type: DataTypes.DATE, allowNull: true}
+    status: {type: DataTypes.STRING, defaultValue: 'active'}
+},
+{
+    timestamps: true,
+    createdAt: "registration_time",
+    updatedAt: "last_login_time",
 })
 
 module.exports = {User}
